15d29948bd90902bd49fca9a03fd5a021fc06134
Lexan <sheklford2015@yandex.ru>
2021-12-17 17:16:04 +0300

    add product db

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from mainapp.models import Product
+
 
 def products(request):
     title = 'каталог'
@@ -10,10 +12,12 @@ def products(request):
         {'href': 'products_modern', 'name': 'модерн'},
         {'href': 'products_classic', 'name': 'классика'},
     ]
+    products_1 = Product.objects.all()[:3]
 
     context = {
         'title': title,
-        'links_menu': links_menu
+        'links_menu': links_menu,
+        'products_1': products_1
     }
     return render(request, 'mainapp/products.html', context=context)
 # Create your views here.
