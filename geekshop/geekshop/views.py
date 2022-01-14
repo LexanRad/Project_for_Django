e6edb4fa4970300e81ee42cd08b457ee54a1edf8
Lexan <sheklford2015@yandex.ru>
2022-01-14 14:08:57 +0300

    delete basket

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 
-from basketapp.models import Basket
 from mainapp.models import Product
 
 
 def index(request):
     title = 'GeekShop'
     products = Product.objects.all()[:4]
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
+
     context = {
         'title': title,
         'products': products,
-        'basket': basket,
     }
     return render(request, 'index.html', context)
 
